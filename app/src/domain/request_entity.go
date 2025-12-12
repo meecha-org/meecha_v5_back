2cef45c3296338760700517caf961e9b3854b9c0
minato0129 <gororyo0129@gmail.com>
2025-12-12 14:38:59 +0900

    entityを定義

---
+++ app/src/domain/request_entity.go
@@ -0,0 +1,23 @@
+package domain
+
+import (
+	"time"
+)
+
+// FriendRequest はフレンドリクエストを表すドメインエンティティ
+type FriendRequest struct {
+	SenderID  string `gorm:"primaryKey"`
+	TargetID  string `gorm:"primaryKey"`
+	RequestID string `gorm:"size:36;uniqueIndex"` // UUIDなどを格納
+	Created   int64  `gorm:"autoCreateTime"`      // 作成時間 (UNIX秒)
+}
+
+// NewFriendRequest は新しいフレンドリクエストを作成します。
+func SendFriendRequest(senderID, targetID, requestID string) *FriendRequest {
+	return &FriendRequest{
+		SenderID:  senderID,
+		TargetID:  targetID,
+		RequestID: requestID, // ID生成はユースケース層で行う
+		Created:   time.Now().Unix(),
+	}
+}
