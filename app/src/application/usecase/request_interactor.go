76ffb11d7335875a6ed402c7301787c32ff48241
minato0129 <gororyo0129@gmail.com>
2026-05-25 11:38:44 +0900

    usecaseではステータスコード返さないように

--- app/src/application/usecase/request_interactor.go
+++ app/src/application/usecase/request_interactor.go
@@ -5,7 +5,7 @@ import (
 	"app/domain"
 	"app/domain/commons/messages"
 	"log"
-	"net/http"
+	
 )
 
 // SendFriendRequestInput はユースケースへの入力データ
@@ -22,50 +22,50 @@ type SendFriendRequestInteractor struct {
 }
 
 // Execute はユースケースを実行します
-func (i *SendFriendRequestInteractor) Execute(input SendFriendRequestInput) (int,error) {
+func (i *SendFriendRequestInteractor) Execute(input SendFriendRequestInput) (error) {
 	// 送信者とターゲットが同一でないか確認
 	if input.SenderID == input.TargetID {
-		return http.StatusBadRequest, messages.ErrSelfRequest
+		return commons.NewBadRequestError("ErrSelfRequest") // 400 Bad Request
 	}
 
 	// 送信者とターゲットが存在するか確認
 	senderExists, err := i.UserRepo.ExistsByID(input.SenderID)
 	if err != nil {
 		log.Println("Error checking sender existence:", err)
-		return http.StatusInternalServerError, err
+		return err
 	}
 	if !senderExists {
-		return http.StatusNotFound, messages.ErrSenderNotFound
-	}
+		return commons.NewBadRequestError("ErrSenderNotFound") // 400 Bad Request
+	} 
 	targetExists, err := i.UserRepo.ExistsByID(input.TargetID)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return err
 	}
 	if !targetExists {
-		return http.StatusNotFound, messages.ErrTargetNotFound
+		return commons.NewBadRequestError("ErrTargetNotFound") // 400 Bad Request
 	}
 
 	// 既にリクエスト済みか確認
 	exists, err := i.Repo.Exists(input.SenderID, input.TargetID)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return	err
 	}	
 	if exists {
-		return http.StatusConflict, messages.ErrAlreadySent
+		return commons.NewConflictError("ErrAlreadySent") // 409 Conflict
 	}
 
 	// uuidを生成
 	uid, err := i.Genid.Genid()
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return err
 	}
 
 	// ドメインエンティティの作成
 	req, err := domain.SendFriendRequest(input.SenderID, input.TargetID, uid)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return err
 	}
 
 	// 永続化
-	return http.StatusCreated, i.Repo.Create(req)
+	return i.Repo.Create(req)
 }
