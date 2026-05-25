b29ceeb153d0d1fb901dbced62b5fe28a2411d66
minato0129 <gororyo0129@gmail.com>
2026-05-25 11:45:42 +0900

    インポート先変更

--- app/src/domain/request_entity.go
+++ app/src/domain/request_entity.go
@@ -17,7 +17,7 @@ type FriendRequest struct {
 func SendFriendRequest(senderID, targetID, requestID string) (*FriendRequest, error) {
 	// 送信者とターゲットが同一でないか確認
 	if senderID == targetID {
-		return nil, messages.ErrSelfRequest
+		return nil, commons.ErrSelfRequest
 	}
 	return &FriendRequest{
 		RequestID: requestID, // ID生成はユースケース層で行う
