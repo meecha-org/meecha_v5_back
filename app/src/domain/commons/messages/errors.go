1d9ab668a14511f822aeae5a382c02a246162e03
minato0129 <gororyo0129@gmail.com>
2026-06-01 10:11:12 +0900

    不要なエラー定義を変更

--- app/src/domain/commons/messages/errors.go
+++ app/src/domain/commons/messages/errors.go
@@ -1,20 +1,9 @@
 package commons
 
-import "errors"
+import
+(
 
-// エラーメッセージの定義(変更不可)
-var (
-	ErrInvalidFormat  = errors.New("フォーマットが不正です")
-	ErrAlreadySent    = errors.New("既にフレンドリクエストが送信されています")
-	ErrSelfRequest    = errors.New("自分自身に送信することはできません")
-	ErrSenderNotFound = errors.New("送信者が存在しません")
-	ErrTargetNotFound = errors.New("ターゲットが存在しません")
 )
-
-//成功
-const (
-	SuccessRequestSent = "フレンドリクエストが送信されました"
-)	
 
 type ErrorType int
 
@@ -45,7 +34,6 @@ func (t ErrorType) String() string {
 	case TypeInternal:
 		return "Internal"
 	}
-
 	return "Unknown"
 }
 
@@ -78,7 +66,6 @@ func NewConflictError(message string) error {
 		Cause:   nil,
 	}
 }
-
 
 func NewBadRequestError(message string) error {
 	return &CustomError{
